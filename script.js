96d23f17c909ca910cc92abb732eaf438f5b7a42
Nathan Laing <ktmlaing@gmail.com>
2024-10-04 12:32:14 +1300

    wip: snake progress 2

--- script.js
+++ script.js
@@ -1,126 +1,94 @@
-const grid = document.querySelector(".grid");
-const playAgain = document.querySelector(".playAgain");
-const gameOver = document.querySelector("#gameOver");
-const scoreDisplay = document.querySelector(".scoreDisplay");
+const BOARD_SIZE = 10;
+const GAME_SPEED = 200; // ms
+const MIDDLE = Math.floor(BOARD_SIZE / 2);
 
-const WIDTH = 10;
-const SPEED = 0.8;
+class Node {
+    val;
+    next;
 
-let currentIndex = 0;
-let appleIndex = 0;
-let currentSnake = [2, 1, 0];
-let direction = 1;
-let score = 0;
-let intervalTime = 0;
-let interval = 0;
-
-function replay() {
-    gameOver.close();
-    grid.innerHTML = "";
-    createBoard();
-    startGame();
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
 }
 
-function control(e) {
-    if (e.defaultPrevented) {
-        return; // Do nothing if event already handled
-    }
+const snake = [new Node({ x: MIDDLE, y: MIDDLE })];
+const food = {
+    x: Math.floor(Math.random() * BOARD_SIZE),
+    y: Math.floor(Math.random() * BOARD_SIZE),
+};
 
-    switch (e.code) {
-        case "KeyS":
-        case "ArrowDown":
-            direction = +WIDTH;
-            break;
+/*
+ * Use linked list for snake
+ * Value at linked list is {x: int, y: int}
+ *
+ * During draw phase we iterate snake and color divs based on x,y
+ *
+ * Food also needs an x,y
+ *
+ * */
 
-        case "KeyW":
+/*
+ * Directions
+ * [0,1] --> Right
+ * [0, -1] --> Left
+ * [1,0] --> Down
+ * [-1, 0] --> Up
+ * */
+
+let board = document.querySelector(".board");
+let score = document.querySelector(".score");
+let direction = "Right";
+
+setInterval(gameLoop, GAME_SPEED);
+document.addEventListener("keydown", handleInput);
+
+function gameLoop() {
+    // move head
+    // move tail
+    // check for collisions
+    // if wall OR tail
+    // reset game
+    // if food
+    // score + 1
+    // generate new food
+    // draw
+}
+
+function handleInput(event) {
+    const key = event.key;
+    switch (key) {
         case "ArrowUp":
-            direction = -WIDTH;
+            direction = "Up";
             break;
-        case "KeyA":
-        case "ArrowLeft":
-            direction = -1;
-            break;
-        case "KeyD":
         case "ArrowRight":
-            direction = 1;
+            direction = "Right";
+            break;
+        case "ArrowDown":
+            direction = "Down";
+            break;
+        case "ArrowLeft":
+            direction = "Left";
             break;
     }
 }
 
-function randomApple(squares) {
-    do {
-        appleIndex = Math.floor(Math.random() * squares.length);
-    } while (squares[appleIndex].classList.contains("snake"));
-    squares[appleIndex].classList.add("apple");
+function reset() {
+    score.innerText = 0;
 }
 
-function eatApple(squares, tail) {
-    if (squares[currentSnake[0]].classList.contains("apple")) {
-        squares[currentSnake[0]].classList.remove("apple");
-        squares[tail].classList.add("snake");
-        currentSnake.push(tail);
-        randomApple(squares);
-        score++;
-        scoreDisplay.textContent = score;
-        clearInterval(interval);
-        intervalTime = intervalTime * SPEED;
-        interval = setInterval(moveOutcome, intervalTime);
+function createBoard(size, gameBoard) {
+    const frag = document.createDocumentFragment();
+
+    for (let i = 0; i < divs.length; i++) {
+        docFrag.appendChild(divs[i]); // Note that this does NOT go to the DOM
+    }
+    document.body.appendChild(docFrag); // Appends all divs at once
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.className = `grid ${i + j}`;
+            gameBoard.append(square);
+        }
     }
 }
-
-function checkForHits(squares) {
-    return (
-        (currentSnake[0] + WIDTH >= WIDTH * WIDTH && direction === WIDTH) ||
-        (currentSnake[0] % WIDTH === WIDTH - 1 && direction === 1) ||
-        (currentSnake[0] % WIDTH === 0 && direction === -1) ||
-        (currentSnake[0] - WIDTH <= 0 && direction === -WIDTH) ||
-        squares[currentSnake[0] + direction].classList.contains("snake")
-    );
-}
-
-function moveSnake(squares) {
-    let tail = currentSnake.pop();
-    squares[tail].classList.remove("snake");
-    currentSnake.unshift(currentSnake[0] + direction);
-    // movement ends here
-    eatApple(squares, tail);
-    squares[currentSnake[0]].classList.add("snake");
-}
-
-function moveOutcome() {
-    let squares = document.querySelectorAll(".grid div");
-    if (checkForHits(squares)) {
-        gameOver.showModal();
-        return clearInterval(interval);
-    } else {
-        moveSnake(squares);
-    }
-}
-
-function startGame() {
-    const squares = document.querySelectorAll(".grid div");
-    randomApple(squares);
-    //random apple
-    direction = 1;
-    scoreDisplay.innerHTML = score;
-    intervalTime = 1000;
-    currentSnake = [2, 1, 0];
-    currentIndex = 0;
-    currentSnake.forEach((index) => squares[index].classList.add("snake"));
-    interval = setInterval(moveOutcome, intervalTime);
-}
-
-function createBoard() {
-    for (let i = 0; i < 100; i++) {
-        grid.appendChild(document.createElement("div"));
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("keyup", control);
-    playAgain.addEventListener("click", replay);
-    gameOver.addEventListener("close", replay);
-
-    createBoard();
-    startGame();
-});
